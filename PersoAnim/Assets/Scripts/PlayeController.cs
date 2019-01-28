1cac120257c42fcc0136216da7e1c1a1563f6266
HuynhCongLap <laphuynh915@gmail.com>
2019-01-28 19:25:00 +0100

    Last

---
+++ PersoAnim/Assets/Scripts/PlayeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayeController : MonoBehaviour
+{
+    Animator myAnimator;
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+        Debug.Log("MyAniConScript: start => Animator");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myAnimator.SetFloat("VSpeed", Input.GetAxis("Vertical"));
+        myAnimator.SetFloat("HSpeed", Input.GetAxis("Horizontal"));
+    }
+}
